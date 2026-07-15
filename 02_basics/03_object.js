1f7525b2e8242ddab74a9a12bb405c8cb2bcf597
Rohitweb1102 <rohitweb1102@gmail.com>
2026-07-15 08:19:42 +0000

    slight change

---
+++ 02_basics/03_object.js
@@ -0,0 +1,16 @@
+//object is a collection of key value pairs
+//what are key and values
+/*const user = {
+
+    name: "Rohit",
+
+    age: 22
+} here name=key and rohit=value*/
+ //methods to create objects 
+ //object literal-ye singleton nhi bnata
+ /*const user = {
+
+}*/
+//constructor methods-ye singleton bnata hai
+// Object.create()or new Object()
+
